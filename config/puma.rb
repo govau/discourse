18b1ca35e12517d29395b277ee72e2d6df379e49
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-02-01 12:48:20 +1100

    Configure puma to work with cf

--- config/puma.rb
+++ config/puma.rb
@@ -10,10 +10,10 @@ if ENV['RAILS_ENV'] == 'production'
 
   # Unless you know what you are changing, do not change them.
   bind "unix://#{APP_ROOT}/tmp/sockets/puma.sock"
-  stdout_redirect "#{APP_ROOT}/log/puma.log", "#{APP_ROOT}/log/puma.err.log"
-  pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
-  state_path "#{APP_ROOT}/tmp/pids/puma.state"
-  daemonize true
+
+  daemonize false
+  port ENV.fetch("PORT") { 3000 }
+
   preload_app!
 
 end
